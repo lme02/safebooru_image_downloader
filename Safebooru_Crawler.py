7adc8137da46911c2f7a3417d245197de812dff1
hunthershysy <hunthershysy@gmail.com>
2022-12-26 20:47:07 +0300

    add requrements

--- Safebooru_Crawler.py
+++ Safebooru_Crawler.py
@@ -1,8 +1,5 @@
 import requests
-import re
 from bs4 import BeautifulSoup
-import urllib.request
-
 
 def spider(max_pages):
     page = 1
@@ -16,9 +13,7 @@ def spider(max_pages):
         for link in soup.find_all("span", attrs={"class":"thumb"}):
             link=str(link.find_all("a")[0].attrs["href"])
             
-            url_pic = "http://safebooru.org/" + link
-            # print (url_pic)
-            
+            url_pic = "http://safebooru.org/" + link         
             source_code_pic = requests.get(url_pic)
             soup1=BeautifulSoup(source_code_pic.content, 'lxml')
             image=soup1.find("img",attrs={"id":"image"}).attrs["src"]
@@ -27,7 +22,6 @@ def spider(max_pages):
             name=image.split('/')[-1].split('?')[0]
             asdf=img_data.find("body")
             print (image)
-            # print(asdf)
             img_data = requests.get(image, verify=False).content
             with open(name, 'wb') as handle:
                 handle.write(img_data)
